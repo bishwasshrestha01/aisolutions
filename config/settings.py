6959249552dcc4f96d105d57c1634084cd83a0bf
Bishwas <bishwasssshrestha.com>
2026-07-01 21:11:08 +0545

    fix STATICFILES_DIRS to point to existing directories

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'css'),
+    os.path.join(BASE_DIR, 'js'),
+    os.path.join(BASE_DIR, 'photos'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
